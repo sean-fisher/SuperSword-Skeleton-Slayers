06da661d4b5a1d29ebe9c1b20f405d0031202691
Thor Keller <thorboomafoo@gmail.com>
2017-10-07 21:20:02 -0400

    Added attacks and structured it to make code more reusable

---
+++ Assets/Scripts/Battle/Chance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Chance : Effect {
+    public float chance;
+    public Effect effect;
+    public override bool AppliesTo(BaseCharacter attacker, BaseCharacter target, int damageDealt, Attack attack)
+    {
+        return effect.AppliesTo(attacker, target, damageDealt, attack) && Random.value < chance;
+    }
+    public override void Apply(BaseCharacter attacker, BaseCharacter target, int damageDealt, Attack attack)
+    {
+        effect.Apply(attacker, target, damageDealt, attack);
+    }
+}
